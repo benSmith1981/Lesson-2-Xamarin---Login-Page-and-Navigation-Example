addfa0845d70fc45050f1229ea873b40bc80e8c1
Ben Smith <benSmith1981@users.noreply.github.com>
2022-03-15 20:45:15 +0000

    Initial commit

---
+++ NavigationExample/NavigationExample/Page2.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NavigationExample
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page2 : ContentPage
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        public void AddToKart(object sender, EventArgs args)
+
+        {
+
+            var button = (Button)sender; //1 
+
+            var classId = button.ClassId; //2 
+
+        }
+    }
+}
